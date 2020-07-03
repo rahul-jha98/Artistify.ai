c17686ec598ddb667db64bc02462cbb7d8c2831c
Rahul Jha <jharahul1998@gmail.com>
2020-07-03 10:37:17 +0530

    Updates

---
+++ precache-manifest.85be1b32025d31f4578114a322318445.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b63ff227f9ed634eeec9ce45661e2e",
+    "url": "/Artistify.ai/index.html"
+  },
+  {
+    "revision": "45616f7af52af72444b8",
+    "url": "/Artistify.ai/static/css/main.856ba215.chunk.css"
+  },
+  {
+    "revision": "538858044944f93bdd70",
+    "url": "/Artistify.ai/static/js/2.b74aaa30.chunk.js"
+  },
+  {
+    "revision": "eed1bb872a3d315d708164461c5cfda5",
+    "url": "/Artistify.ai/static/js/2.b74aaa30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45616f7af52af72444b8",
+    "url": "/Artistify.ai/static/js/main.3e0d6b8c.chunk.js"
+  },
+  {
+    "revision": "72653139ee1cb0fdf7b3",
+    "url": "/Artistify.ai/static/js/runtime-main.88a360a8.js"
+  },
+  {
+    "revision": "f4b040c39cb113d9e72e475fab81d24e",
+    "url": "/Artistify.ai/static/media/logotext.f4b040c3.svg"
+  }
+]);
