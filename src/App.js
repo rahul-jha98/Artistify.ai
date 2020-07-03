0e93218cbc00f831f070b7319bc654296f76df5a
Rahul Jha <jharahul1998@gmail.com>
2020-07-03 10:26:37 +0530

    Add basic selection page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import HomeLite from './components/Home/HomeLite';
 import HomePro from './components/Home/HomePro';
 
 function App() {
+  const [page, setPage] = React.useState(0);
+  console.log(page);
   return (
     <div className="App">
-      <HomePro/>
+      {
+        page === 0 ? 
+        <React.Fragment>
+          <button onClick={() => setPage(1)}>Lite Mode</button>
+          <button onClick={() => setPage(2)}>Pro Mode</button>
+        </React.Fragment>
+        :
+        page === 1?
+        <HomeLite/>:
+        <HomePro/>
+      }
     </div>
   );
 }
